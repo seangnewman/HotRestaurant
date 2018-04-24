825629f92ec78f0b4cd8c76c12be56105cacef05
Ricky Hsu <rickyhsu0101@gmail.com>
2018-04-23 19:13:29 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require ("express");
+
+
+
+var server = express();
+
+server.get("/home", function(req, res){
+
+});
+server.get("/view", function(req, res){
+
+});
+server.get("/make", function(req, res){
+
+});
+server.get("/api/tables", function(req, res){
+
+});
+server.post("/api/reserve/:reservations", function(req, res){
+
+});
+
+//
