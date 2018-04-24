8f74878b030593101a5691cfa7768bad00cb6ade
Ricky Hsu <rickyhsu0101@gmail.com>
2018-04-23 20:44:39 -0700

    finished

--- js/tables.js
+++ js/tables.js
@@ -3,9 +3,12 @@ $(document).ready(function(){
         url: "/api/tables",
         method: "GET"
     }).done(function(res){
+        for(var i = 0; i < res.length; i++){
+
+        }
         res.forEach(function(reservation){
             if(reservation.seated == true){
-                
+                $("#tableSection").append();
             }else{
 
             }
