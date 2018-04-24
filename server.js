b10f5e8bc6a675400f5f90669e779c489b71fba8
Trevor Johnson <johnsontrevor55@gmail.com>
2018-04-23 20:59:34 -0700

    adjusted route to post userObj

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-var express = require ("express");
-var path = require("path")
+var express = require("express");
+var path = require("path");
 var server = express();
 var bodyParser = require("body-parser");
 
 const PORT = 8080;
-
 
 var reservations = [];
 //middleware
@@ -13,33 +12,34 @@ server.use(bodyParser.json());
 server.use("/static", express.static(path.join(__dirname, "./js")));
 
 //routes
-server.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "/index.html"));
+server.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
-server.get("/reserve", function(req, res){
-    res.sendFile(path.join(__dirname, "/reservation.html"));
+server.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "/reservation.html"));
 });
-server.get("/tables", function(req, res){
-    res.sendFile(path.join(__dirname, "/tables.html"));
+server.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "/tables.html"));
 });
-server.get("/api/tables", function(req, res){
-    res.json(reservations);
+server.get("/api/tables", function(req, res) {
+  res.json(reservations);
 });
-server.post("/api/reserve/:reservation", function(req, res){
-    var reservation = req.params.reservation;
-    console.log(reservation)
-    if(reservations.length > 5){
-        reservation.seated = false;
-    }else{
-        reservation.seated = true;
-    }
-    reservations.push(reservation);
+server.post("/api/reserve/", function(req, res) {
+  var reservation = req.body;
+  console.log(req.body);
+  if (reservations.length > 5) {
+    reservation.seated = false;
+  } else {
+    reservation.seated = true;
+  }
+  reservations.push(reservation);
+  res.end();
 });
-server.post("/api/clear", function(req, res){
-    reservations = [];
+server.post("/api/clear", function(req, res) {
+  reservations = [];
 });
 
 //initialize server
-server.listen(PORT, function(){
-    console.log("Listening on port " + PORT);
+server.listen(PORT, function() {
+  console.log("Listening on port " + PORT);
 });
