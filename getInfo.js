be947f49df215826b55cd82fc88012a2b2e1868e
Trevor Johnson <johnsontrevor55@gmail.com>
2018-04-23 20:04:33 -0700

    added clear form function

--- getInfo.js
+++ getInfo.js
@@ -4,11 +4,18 @@ var reserve_email = document.getElementById("reserve_email");
 var reserve_uniqueID = document.getElementById("reserve_uniqueID");
 var res_submit = document.getElementById("res_submit");
 
+function clearAll() {
+  reserve_name.value = "";
+  reserve_phone.value = "";
+  reserve_email.value = "";
+  reserve_uniqueID.value = "";
+}
+
 res_submit.onclick = function() {
-  var reserve_name_val = reserve_name.value().trim();
-  var reserve_phone_val = reserve_phone_val.value().trim();
-  var reserve_email_val = reserve_email_val.value().trim();
-  var reserve_uniqueID_val = reserve_email_val.value().trim();
+  var reserve_name_val = reserve_name.value.trim();
+  var reserve_phone_val = reserve_phone_val.value.trim();
+  var reserve_email_val = reserve_email_val.value.trim();
+  var reserve_uniqueID_val = reserve_email_val.value.trim();
 
   var userObj = {
     reserve_name_val,
@@ -16,4 +23,10 @@ res_submit.onclick = function() {
     reserve_email_val,
     reserve_uniqueID_val
   };
+
+  $.ajax({ method: "POST", url: "/api/reserve", data: userObj }).done(
+    function() {
+      clearAll();
+    }
+  );
 };
